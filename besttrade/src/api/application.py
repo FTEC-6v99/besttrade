ab3a7d8d3184f8d455e0d18a808e33b8def5124e
Mohamad Bourji <mohd.bourji@gmail.com>
2021-11-02 18:42:44 -0500

    add logger and temporarily disable deploy condition

--- besttrade/src/api/application.py
+++ besttrade/src/api/application.py
@@ -1,3 +1,6 @@
+import logging
+import datetime
+import pytz
 from flask import Flask
 from besttrade.src.api.blueprints.investorbp import investor_blueprint
 
@@ -5,9 +8,12 @@ application = app = Flask(__name__)
 
 @app.route('/')
 def health_check():
-    return 'OK'
+    date_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+    application.logger.info("The application is healthy. call received at: " + str(date_time))
+    # print(f'The date time now is: {str(date_time)}')
+    return f'OK: {str(date_time)}'
 
 app.register_blueprint(investor_blueprint)
 
 if __name__ == '__main__':
-    application.run()
+    application.run(debug=True)
